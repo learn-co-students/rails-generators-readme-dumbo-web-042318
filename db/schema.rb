094ec1810091237282c5a203cfedfea63dae5844
snkoski <shawnnkoski@gmail.com>
2018-05-24 14:39:11 -0400

    generators, following directions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180524181814) do
+ActiveRecord::Schema.define(version: 20180524183831) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "payment_status"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +40,7 @@ ActiveRecord::Schema.define(version: 20180524181814) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string   "post_status"
   end
 
 end
